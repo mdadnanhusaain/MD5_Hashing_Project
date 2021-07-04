3be411156265c7936193529e0f1b485b136b4167
Md Adnan Hussain <83577566+mdadnanhusaain@users.noreply.github.com>
2021-07-04 12:06:06 +0530

    Update Python and Network Security Final Project.py

--- Python and Network Security Final Project.py
+++ Python and Network Security Final Project.py
@@ -1,8 +1,5 @@
-import os
-import hashlib
-file_location = os.path.abspath("md5 encoding.txt")
-hasher = hashlib.md5()
-with open(file_location,'rb') as file:
-    content = file.read()
-    hasher.update(content)
-print(hasher.hexdigest())
+import hashlib       # importing hashlib package to perform md5 encryption
+hashing = input("Enter the string to be md5 hashed:- ")       # accepting the string from user to be hashed
+hashed = hashlib.md5(hashing.encode())       #hashing the string 
+print("The byte equivalent of hash is : ", hashed.digest())       # printing the Byte Equivalent of Hash
+print("The hexadecimal equivalent of hash is : ", hashed.hexdigest())       # printing the Hexadecimal Equivalent of Hash
