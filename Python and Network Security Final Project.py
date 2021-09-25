67ba45c6ab067bb0057a8c1095dc3243e5fb9581
mdadnanhusaain <mdadnanhusaain@gmail.com>
2021-09-25 17:25:36 +0530

    Renamed files

--- Python and Network Security Final Project.py
+++
@@ -1,5 +0,0 @@
-import hashlib       # importing hashlib package to perform md5 encryption
-hashing = input("Enter the string to be md5 hashed:- ")       # accepting the string from user to be hashed
-hashed = hashlib.md5(hashing.encode())       #hashing the string 
-print("The byte equivalent of hash is : ", hashed.digest())       # printing the Byte Equivalent of Hash
-print("The hexadecimal equivalent of hash is : ", hashed.hexdigest())       # printing the Hexadecimal Equivalent of Hash
